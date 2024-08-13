02fb4f92a04a45bb15094c33caa7c91b37a51ea2
Aditya Oberai <adityaoberai1@gmail.com>
2024-08-13 16:18:56 +0000

    Update constants to Day 0

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,6 +2,6 @@ export const APPWRITE_ENDPOINT = 'https://cloud.appwrite.io/v1';
 
 export const APPWRITE_PROJECT = 'initraffle';
 
-export const DATABASE_NAME = 'raffledb';
+export const DATABASE_NAME = 'init-2-db';
 
-export const COLLECTION_NAME = 'day4';
+export const COLLECTION_NAME = 'day0';
